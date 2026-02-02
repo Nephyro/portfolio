33bbb81fdb3d431c412adc778437c48387fd1a6b
Nephyro <anderson.soare.4245@gmail.com>
2026-02-02 11:01:18 -0300

    adicionando fucionalidade modo escuro

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
 const closeSidebar = document.getElementById("closeSidebar");
 const overlay = document.getElementById("overlay");
+const tema = document.getElementById("botaoTema");
+
+botaoTema.addEventListener("click", () => {
+    document.body.classList.toggle("modo-escuro");
+});
 
 // Abre a sidebar
 menuBtn.addEventListener("click", () => {
